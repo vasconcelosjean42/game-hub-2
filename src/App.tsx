241534a6653e915e30e2a9f8b4f468b70bc15a92
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2024-01-29 21:07:34 -0300

    Initial structure of the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button variant={"destructive"}>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
