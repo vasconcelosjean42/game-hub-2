4e02fda7492291a3a3f6c993f5b3cc6dafa098d0
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2024-01-31 21:03:54 -0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { Button } from "./components/ui/button";
 function App() {
   return (
     <>
-      <div>
-        <Button variant={"destructive"}>Click me</Button>
+      <div className="w-full">
+        <div className="nav bg-green-400">nav</div>
+        <div className="flex">
+          <div className="aside w-full hidden lg:block bg-blue-400">aside</div>
+          <div className="main w-full bg-red-400">main</div>
+        </div>
       </div>
     </>
   );
